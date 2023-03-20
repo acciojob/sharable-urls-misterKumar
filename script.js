c07bd830634a6bd56bd8a683f4bfde4fecd7e35e
misterKumar <120304503+misterKumar@users.noreply.github.com>
2023-03-20 10:54:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // your code here
+const form = document.querySelector('form');
+const url = document.querySelector('#url');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  const name = encodeURIComponent(document.querySelector('#name').value);
+  const year = encodeURIComponent(document.querySelector('#year').value);
+  
+  url.textContent = `https://localhost:8080/?name=${name}&year=${year}`;
+});
